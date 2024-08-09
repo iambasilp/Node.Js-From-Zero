c00db8319c9ab5270efed2c9b11533b17b2dbce7
iambasilp <iambasilp@gmail.com>
2024-08-09 19:48:41 +0530

    Create server and parcticed simple way

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,43 +1,19 @@
-const { error } = require('console');
-const fs = require('fs')
 const http = require('http')
 const url = require('url')
-
-const ReadData = fs.readFile('./txt/basil.txt','utf-8',(error,data)=>{
-    console.log("Hello")
-})
-
+const fs  = require('fs')
 const server = http.createServer((req,res)=>{
     const pathName = req.url
-    switch(pathName){
-        case "/contact":
-        res.end("This is Contact page");
-
-
-        case "/about":
-         res.end("This is About page");
-    
-
-        case "/service":
-         res.end("This is service page");
-   
-
-         default:
-         fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(error,data)=>{
-            res.writeHead(200,{'Content-type':'application/json'});
-            res.end(data)
-            console.log(data);
-         })
-      
-
+    if(pathName === '/products'){
+        fs.readFile('./dev-data/productData.json','utf-8',(error,data)=>{
+             res.writeHead('200',{'Content-type':'application/json'})
+             res.end(data)
+        })
+    }else{
+        res.end("This is from Serveroo")
     }
-    
-
 })
 
 server.listen('7000','127.0.0.1',()=>{
-console.log("Server is running"
-
-)})
-
+    console.log("Listening on 7000")
+})
 
