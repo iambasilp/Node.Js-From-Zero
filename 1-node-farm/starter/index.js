8b196ab2f3794bdae645cf91a2f43722aebad90e
iambasilp <iambasilp@gmail.com>
2024-08-10 16:18:53 +0530

    Slugify Package used

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const slugify = require("slugify")
 const querystring = require("querystring");
 const replaceTemplate = require("./modules/replaceTemplate")
 
@@ -20,18 +21,29 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+
+const slugs = dataObj.map((el)=>slugify(el.productName,{lower:true}))
+console.log(slugify('Fresh Avocados',{lower:true}))
+
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url);
+  
   const parsedQuery = querystring.parse(query);
 
+
+  // api
   if (pathname === "/api") {
     res.writeHead("200", { "Content-type": "application/json" });
     res.end(data);
+
+  // overview
   } else if (pathname === "/overview" || pathname === "/") {
     res.writeHead("200", { "Content-type": "text/html" });
     const CardHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", CardHtml);
     res.end(output);
+
+  // product
   } else if (pathname === "/product") {
     res.writeHead("200", { "Content-type": "text/html" });
     const product = dataObj[parsedQuery.id];
@@ -43,6 +55,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(2000, () => {
-  console.log("Listening on port 2000");
+server.listen(4000, () => {
+  console.log("Listening on port 4000");
 });
